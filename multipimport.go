e8153db8d2e887c826e00c026bc9b43e23f7de32
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-12-03 11:13:51 +0530

    multipleimport.go
    multiple packages

---
+++ multipimport.go
@@ -0,0 +1,40 @@
+Importing Multiple Packages
+Previously, we imported a single package, fmt. But, we can import so many more! Go has an extensive list of packages that we can take advantage of. Here’s a list of Go’s standard packages
+
+To import multiple packages we can add multiple import statements, like so:
+
+import "package1"
+import "package2"
+Or we can use a single import with a pair of parentheses that contain our packages:
+
+import (
+  "package1"
+  "package2"
+)
+Notice, when using an import with parentheses, we’re not separating each package with a comma. Instead, each package is on a different line.
+
+We can also provide an alias to a package by including the alias name before the file. Including an alias will make it easier to refer to the package without typing out the full package name:
+
+import (
+  p1 "package1"
+  "package2"
+)
+In the example above we’ve aliased package1 as p1 and now we can call functions from package1 by using p1 like:
+
+p1.SampleFunc()
+Instead of:
+
+package1.SampleFunc() 
+
+
+package main
+
+import ( 
+  "fmt"
+  t "time" 
+)
+
+func main() {
+	fmt.Println("Hello World")
+  fmt.Println(t.Now())
+}
